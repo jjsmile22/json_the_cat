f6609589030f9072bb2cb8c3ed816d277a113ad8
Jordan Gertenstein <jordangert@hotmail.com>
2022-09-01 15:46:56 -0500

    renamed breedfetcher

--- breedfetcher.js
+++ breedfetcher.js
@@ -5,41 +5,33 @@ const breedName = myArgs[0];
 
 const request = require('request');
 
-// const fs = require('fs');
-
 const fetchPagePutIntoDoc = function(breedName) {
   console.log('Requesting data from URL...');
+  //requesting breed name
   request("https://api.thecatapi.com/v1/breeds/search?name=" + breedName, (error, response, body) => {
-  if (error){
-    return console.log("error:",  error)
-  }
+  //catch error  
+  if (error) {
+      return console.log("error:", error);
+    }
+  //parse body into javascript object
     const data = JSON.parse(body);
-    let breed = data[0]
+    let breed = data[0];
     if (breed) {
-      console.log(breed.description)
+      console.log(breed.description);
+  //if breed does not exist write message
     } else {
-      console.log("unable to find a breed for" + breedName)
+      console.log("unable to find a breed for" + breedName);
       return;
     }
-    
-    
+
+
     console.log(typeof data);
     console.log(typeof body);
-    
-    
+
+
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // Print the HTML for the Google homepage.
-
-    // const textToAppend = body;
-
-    // fs.appendFile(`./data/${fileName}.txt`, textToAppend, err => {
-    //   if (err) {
-    //     console.error(err);
-    //   }
-    //   console.log('Data appended to file');
-    //   console.log(`Downloaded and saved ${textToAppend.length} bytes to ${fileName}`)
-    // });
+    
   });
 };
 
