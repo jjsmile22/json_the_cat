274544b3deb96f028b6c35ef2b13659d50608145
Jordan Gertenstein <jordangert@hotmail.com>
2022-09-01 20:40:07 -0500

    refactored with Ahana, Michael and Cindy

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,38 +1,33 @@
-const myArgs = process.argv.slice(2);
-console.log('myArgs: ', myArgs);
-
-const breedName = myArgs[0];
-
 const request = require('request');
 
-const fetchPagePutIntoDoc = function(breedName) {
+
+const fetchBreedDescription = function(breedName, callback) {
   console.log('Requesting data from URL...');
   //requesting breed name
   request("https://api.thecatapi.com/v1/breeds/search?name=" + breedName, (error, response, body) => {
-  //catch error  
-  if (error) {
-      return console.log("error:", error);
+    //catch error
+    if (error) {
+      callback(error, null);
+      return;
     }
-  //parse body into javascript object
+    //parse body into javascript object
     const data = JSON.parse(body);
     let breed = data[0];
     if (breed) {
-      console.log(breed.description);
-  //if breed does not exist write message
+      callback(null, breed.description);
+
+    //if breed does not exist write message
     } else {
-      console.log("unable to find a breed for" + breedName);
+      callback("unable to find a breed for", null);
       return;
     }
 
-
     console.log(typeof data);
     console.log(typeof body);
-
-
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    
+
   });
 };
 
-fetchPagePutIntoDoc(breedName);
+module.exports = { fetchBreedDescription };
